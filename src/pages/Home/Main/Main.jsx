94ab567c29643b1f6d2eb6460539d7ad27ea8def
Samuel-Sales <cacabrbf@hotmail.com>
2024-02-20 21:49:59 -0300

    Main - Feita de forma..., texto, link para modo de preparo pronto

--- src/pages/Home/Main/Main.jsx
+++ src/pages/Home/Main/Main.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Botao from "../../../components/Botao/Botao";
 
 export default function Main() {
@@ -9,6 +10,15 @@ export default function Main() {
                     A culinária Japonesa é bastante equilibrada, sendo muito rica em peixes (ômega 3), vegetais, massas e ingredientes frescos.
                 </p>
                 <Botao />
+                <img className="mt-12 ml-6 mr-auto" src="/imagemSegurandoPrato.svg" alt="Imagem segurando o prato" />
+            </section>
+
+            <section className="mt-14">
+                <h2 className="font-semibold text-2xl text-center text-custom-black mb-6">Feita de forma Tradicional</h2>
+                <p className="font-normal text-base text-center ml-3 mr-3 mb-6">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <Link className="text-custom-red border-b border-custom-red font-medium text-base ml-12 mb-14" to=''>Ler mais sobre o modo de preparo</Link>
             </section>
         </>
     );
